f72cc527eed4117971b5b82aa6b2f852d4f22d21
SSri-kanth <53237072+SSri-kanth@users.noreply.github.com>
2020-07-03 00:07:44 +0530

    Add files via upload

---
+++ key generation for location.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  5 11:45:33 2019
+
+@author: SRIKANTH
+"""
+
+from  geopy.geocoders import Nominatim
+
+geolocator = Nominatim()
+
+Place = str(input("enter your location: "))
+location = geolocator.geocode(Place)
+
+latitude = str(location.latitude)
+longitude = str(location.longitude)
+k1 = latitude[0:2] + longitude[3:]
+k2 = longitude[0:2] + latitude[3:]
+k = k1 + k2
+print(k[0:16])
